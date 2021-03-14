e1ca78da2f48a48eb67c815ac98c6caed5259103
RTBlanco <ronnytoribio1@hotmail.com>
2021-03-14 03:51:40 -0400

    added the routing to home page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,10 +18,18 @@ class Users::SessionsController < Devise::SessionsController
     super
   end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   end
+
+  def after_sign_in_path_for(resource)
+    user_home_path(resource)
+  end
+
+  def respond_to_on_destroy
+    redirect_to new_user_session_path
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root "users#home"
+  
+  root "home#index"
+  
 
   
-  # get '/user/home/', to: "users#home"
-  get '/user/browse/', to: "users#browse"
+  get '/user/home/', to: "home#home"
+  get '/user/browse/', to: "home#browse"
   # resource :user, only: [:home, :browse]
   # resolve("User") {[:user]}
   # devise_for :users
   # devise_for :users, skip: :all #, controllers: {sessions: 'users/sessions'}
   
-  resources :users, only: [:home, :browse]
+  
 
   devise_for :users, skip: [:confirmations, :mailers], skip_helpers: true,
              path_names: { sign_in: "login", sign_out: 'logout'},
